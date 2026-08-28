d46d702b6aa0b3d0fcd0d174c341e275e4468f47
amyfallowfield <amyfallowfield123@gmail.com>
2026-08-28 20:01:00 +0100

    test: Add ArtistStatsManagerTest

--- src/services/src/ArtistStatsManager.cpp
+++ src/services/src/ArtistStatsManager.cpp
@@ -1,4 +1,6 @@
+#include <iomanip>
 #include <iostream>
+#include <math.h>
 #include <vector>
 
 #include "ArtistStatsManager.h"
@@ -15,14 +17,18 @@ void ArtistStatsManager::print_stats(const std::vector<Artist>& artists)
     for (const Artist& artist : artists)
     {
         std::cout << "Name: " << artist.get_name() << '\n';
-        std::cout << "First Seen: "<< artist.get_first_seen() << '\n';
-        std::cout << "Last Seen: "<< artist.get_last_seen() << '\n';
-        std::cout << "Times Seen: "<< artist.get_count() << '\n';
-        std::cout << "Average Cost: £"<< average_cost(artist) / 100 << '\n\n';
+        std::cout << "First Seen: " << artist.get_first_seen() << '\n';
+        std::cout << "Last Seen: " << artist.get_last_seen() << '\n';
+        std::cout << "Times Seen: " << artist.get_count() << '\n';
+        std::cout << "Average Cost: £"
+                  << std::fixed
+                  << std::setprecision(2)
+                  << average_cost(artist) / 100.0f
+                  << '\n';
     }
 }
 
 int32_t ArtistStatsManager::average_cost(const Artist& artist)
 {
-    return artist.get_total_cost() / artist.get_count();
+    return std::round(static_cast<double>(artist.get_total_cost()) / artist.get_count());
 }
---
+++ tests/services/ArtistStatsManagerTest.cpp
@@ -0,0 +1,92 @@
+#include <gtest/gtest.h>
+
+#include "Artist.h"
+#include "ArtistStatsManager.h"
+#include "Concert.h"
+#include "TestData.h"
+
+class ArtistStatsManagerTest : public testing::Test
+{
+public:
+    std::ostringstream output;
+
+    std::streambuf* original_cout;
+
+    void SetUp() override
+    {
+        original_cout = std::cout.rdbuf();
+
+        std::cout.rdbuf(output.rdbuf());
+    }
+
+    void TearDown() override
+    {
+        std::cout.rdbuf(original_cout);
+    }
+};
+
+TEST_F(ArtistStatsManagerTest, print_empty_artists)
+{
+    ArtistStatsManager manager = ArtistStatsManager();
+
+    manager.print_stats({});
+
+    ASSERT_NE(std::string::npos, output.str().find("No statistics to show")) << "Print stats should inform user of no statistics when no artists";
+}
+
+TEST_F(ArtistStatsManagerTest, print_one_artist_one_concert)
+{
+    ArtistStatsManager manager = ArtistStatsManager();
+
+    Concert concert = TestData::create_test_concert1();
+    Artist artist = TestData::create_test_artist("Benjamin Steer", {concert});
+
+    manager.print_stats({artist});
+
+    ASSERT_NE(std::string::npos, output.str().find("Name: Benjamin Steer")) << "Print stats should print artist name";
+    ASSERT_NE(std::string::npos, output.str().find("First Seen: 23-05-2026")) << "Print stats should print first seen date";
+    ASSERT_NE(std::string::npos, output.str().find("Last Seen: 23-05-2026")) << "Print stats should print last seen date";
+    ASSERT_NE(std::string::npos, output.str().find("Times Seen: 1")) << "Print stats should print times seen";
+    ASSERT_NE(std::string::npos, output.str().find("Average Cost: £20")) << "Print stats should print average cost";
+}
+
+TEST_F(ArtistStatsManagerTest, print_one_artist_two_concerts)
+{
+    ArtistStatsManager manager = ArtistStatsManager();
+
+    Concert concert1 = TestData::create_test_concert1();
+    Concert concert2 = TestData::create_test_concert2();
+    Artist artist = TestData::create_test_artist("Benjamin Steer", {concert1, concert2});
+
+    manager.print_stats({artist});
+
+    ASSERT_NE(std::string::npos, output.str().find("Name: Benjamin Steer")) << "Print stats should print artist name";
+    ASSERT_NE(std::string::npos, output.str().find("First Seen: 23-05-2026")) << "Print stats should print first seen date";
+    ASSERT_NE(std::string::npos, output.str().find("Last Seen: 30-05-2026")) << "Print stats should print last seen date";
+    ASSERT_NE(std::string::npos, output.str().find("Times Seen: 2")) << "Print stats should print times seen";
+    ASSERT_NE(std::string::npos, output.str().find("Average Cost: £22.50")) << "Print stats should print average cost";
+}
+
+TEST_F(ArtistStatsManagerTest, print_two_artist_two_concerts)
+{
+    ArtistStatsManager manager = ArtistStatsManager();
+
+    Concert concert1 = TestData::create_test_concert1();
+    Concert concert2 = TestData::create_test_concert2();
+    Artist main_artist = TestData::create_test_artist("Benjamin Steer", {concert1, concert2});
+    Artist support_artist = TestData::create_test_artist("Dolder", {concert1});
+
+    manager.print_stats({main_artist, support_artist});
+
+    ASSERT_NE(std::string::npos, output.str().find("Name: Benjamin Steer")) << "Print stats should print artist name";
+    ASSERT_NE(std::string::npos, output.str().find("First Seen: 23-05-2026")) << "Print stats should print first seen date";
+    ASSERT_NE(std::string::npos, output.str().find("Last Seen: 30-05-2026")) << "Print stats should print last seen date";
+    ASSERT_NE(std::string::npos, output.str().find("Times Seen: 2")) << "Print stats should print times seen";
+    ASSERT_NE(std::string::npos, output.str().find("Average Cost: £22.50")) << "Print stats should print average cost";
+
+    ASSERT_NE(std::string::npos, output.str().find("Name: Dolder")) << "Print stats should print artist name";
+    ASSERT_NE(std::string::npos, output.str().find("First Seen: 23-05-2026")) << "Print stats should print first seen date";
+    ASSERT_NE(std::string::npos, output.str().find("Last Seen: 23-05-2026")) << "Print stats should print last seen date";
+    ASSERT_NE(std::string::npos, output.str().find("Times Seen: 1")) << "Print stats should print times seen";
+    ASSERT_NE(std::string::npos, output.str().find("Average Cost: £20.00")) << "Print stats should print average cost";
+}
